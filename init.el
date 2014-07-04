9a40ffb6f1022dbda9a7f4ed91623db04a6879c1
Louis Chan <louisch@hacari.com>
2014-07-04 15:04:03 +0800

    Remove trailing whitespace

--- init.el
+++ init.el
@@ -55,6 +55,8 @@ locate PACKAGE."
 (add-to-list 'default-frame-alist '(fullscreen . fullheight))
 ; Turn column numbers on in the modeline
 (setq column-number-mode t)
+; Show trailing whitespace
+(setq show-trailing-whitespace t)
 
 ; Behaviour
 ; Focus the initial window on startup
