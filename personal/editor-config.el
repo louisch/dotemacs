61a95c34ec891c7c682857076d2fbb284c6e9766
Louis Chan <louisch@hacari.com>
2014-11-05 00:56:58 +0000

    Make tab width smaller

--- personal/editor-config.el
+++ personal/editor-config.el
@@ -41,8 +41,8 @@
 
 ;; Indentation
 (setq-default indent-tabs-mode nil) ; Spaces only for indentation
-(setq tab-width 4
-      c-basic-offset 4)
+(setq tab-width 2
+      c-basic-offset 2)
 
 ;; Set an absolute backup directory, placing it in the emacs config dir
 (setq backup-directory-alist
