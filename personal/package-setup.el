448faaba6bc91462501599fa48bde44542299220
Louis Chan <louisch@hacari.com>
2014-11-05 00:09:26 +0000

    Create directory for modularizing personal config
    Start by moving package setup code outside init.el into personal/

---
+++ personal/package-setup.el
@@ -0,0 +1,82 @@
+;;; -*- lexical-binding: t -*-
+
+;;; Packages
+
+(require 'cl)
+(require 'package)
+;; List of package archives
+                                        ; (add-to-list 'package-archives
+                                        ;              '("marmalade" . "https://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+;; Initialise packages
+(package-initialize)
+(package-refresh-contents)
+
+
+;; Functions for installing and managing packages taken from Emacs Prelude
+(defun packages-installed-p ()
+  "Check if all packages in `my-packages' are installed."
+  (every #'package-installed-p my-packages))
+(defun require-package (package)
+  "Install PACKAGE unless already installed."
+  (unless (memq package my-packages)
+    (add-to-list 'my-packages package))
+  (unless (package-installed-p package)
+    (package-install package)))
+(defun require-packages (packages)
+  "Ensure PACKAGES are installed.
+Missing packages are installed automatically."
+  (mapc #'require-package my-packages))
+(defun try-install-packages ()
+  "Install any packages listed in `my-packages' if not installed."
+  (unless (packages-installed-p)
+    ;; check for new packages (package versions)
+    (message "%s" "Now refreshing package database...")
+    (package-refresh-contents)
+    (message "%s" " done.")
+    ;; install the missing packages
+    (require-packages my-packages)))
+
+;; Required packages:
+(defvar my-packages
+  '(ace-jump-mode
+    ack-and-a-half
+    aggressive-indent
+    anzu
+    auto-complete
+    auctex
+    evil
+    evil-leader
+    evil-paredit
+    flx-ido
+    flycheck
+    git-timemachine
+    gitignore-mode
+    helm
+    helm-projectile
+    linum-relative
+    magit
+    paredit
+    powerline
+    powerline-evil
+    projectile
+    saveplace
+    smartparens
+    solarized-theme
+    volatile-highlights
+    yasnippet
+
+    csharp-mode
+
+    clojure-mode
+    clojure-test-mode
+    cider
+
+    haskell-mode
+
+    markdown-mode)
+  "A list of packages to ensure are installed at launch")
+;; Install all above packages
+(try-install-packages)
