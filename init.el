34d6da696c36f8f19053597ba768be1373186ca3
Louis Chan <louisch@hacari.com>
2014-11-05 01:49:32 +0000

    Split line at 80th column

--- init.el
+++ init.el
@@ -2,7 +2,8 @@
 
 (require 'cl)
 (defvar personal-dir (concat user-emacs-directory "personal")
-  "Directory containing personal configuration files to load at initialization.")
+  "Directory containing personal configuration files to load at
+initialization.")
 (add-to-list 'load-path personal-dir)
 (load "package-setup.el")
 (load "package-config.el")
