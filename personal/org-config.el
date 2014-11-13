019359c0f99d92ab78c1a28be6f0268bd29ff975
Louis Chan <louisch@hacari.com>
2014-11-13 20:09:51 +0000

    Reorder effort columns so that minutes are last in list

--- personal/org-config.el
+++ personal/org-config.el
@@ -93,7 +93,7 @@ some point.")
                          ,someday-file))
 ;; Global Properties
 (setq org-global-properties
-      '((Effort_ALL "0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00")))
+      '((Effort_ALL "1:00 2:00 3:00 4:00 5:00 6:00 7:00 0:10 0:30")))
 (setq org-columns-default-format
       (concat "%25ITEM(Task) %TODO %TAGS(Context) "
               "%17Effort(Estimated Effort){:} %CLOCKSUM"))
