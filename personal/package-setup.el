03687bffe53b8fa1b7b01a4955ca4842041ba410
Louis Chan <louis058@gmail.com>
2014-12-22 18:02:04 +0000

    Enable gnu package archive when below version 24

--- personal/package-setup.el
+++ personal/package-setup.el
@@ -9,6 +9,9 @@
                                         ;              '("marmalade" . "https://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
 ;; Initialise packages
 (package-initialize)
