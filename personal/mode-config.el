5611d5ab026b7b539870ea4c35a0eec90bef6ddc
Louis Chan <louis058@gmail.com>
2015-06-16 15:08:20 +0100

    Make web mode compatible with smartparens

--- personal/mode-config.el
+++ personal/mode-config.el
@@ -57,3 +57,14 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+; Code to make web-mode compatible with smartparens
+(defun my-web-mode-hook ()
+  (setq web-mode-enable-auto-pairing nil))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+(defun sp-web-mode-is-code-context (id action context)
+  (when (and (eq action 'insert)
+             (not (or (get-text-property (point) 'part-side)
+                      (get-text-property (point) 'block-side))))
+
+    t))
+(sp-local-pair 'web-mode "<" nil :when '(sp-web-mode-is-code-context))
